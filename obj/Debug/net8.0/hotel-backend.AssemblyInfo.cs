e48e233c6e8d9c1fa5404276c188adfb42ac879d
DasonTiovino <114289111+DasonTio@users.noreply.github.com>
2024-05-26 15:32:33 +0700

    fix: git rebase

--- obj/Debug/net8.0/hotel-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/hotel-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hotel-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3ccffb89c230f6e66a62c7b2aad5f3e4e6eefdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31a8adf9055b1443738674f9611c763f93aaa3e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("hotel-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hotel-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
